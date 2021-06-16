6bcc841f4f0d6468e5548aa3a0d3a17cb39be7d1
Dimitry Medhat <TonyMedhat5@tutanota.com>
2021-06-16 17:26:27 +0200

    Updated PlayerMovement
    Fixed Rotation Movement Bug

--- Assets/Scripts/Core/PlayerMovement.cs
+++ Assets/Scripts/Core/PlayerMovement.cs
@@ -1,14 +1,14 @@
-using System;
 using UnityEngine;
 
 namespace DarkKey.Core
 {
+    [RequireComponent(typeof(Rigidbody), typeof(InputHandler))]
     public class PlayerMovement : MonoBehaviour
     {
         private Rigidbody _rb;
         private InputHandler _inputHandler;
         [SerializeField] private float moveSpeed;
-        
+
 #region Unity Methods
         
         private void Start()
@@ -17,16 +17,27 @@ namespace DarkKey.Core
             _inputHandler = GetComponent<InputHandler>();
         }
 
-        private void FixedUpdate()
-        {
-            Move(_inputHandler.InputVector);
-        }
+        private void FixedUpdate() => Move(_inputHandler.InputVector);
 
-        #endregion
+#endregion
+
+#region Private Methods
+
         private void Move(Vector3 inputDirection)
         {
-            var inputDir = new Vector3(inputDirection.x, 0, inputDirection.y);
-            _rb.MovePosition(transform.position + inputDir * (moveSpeed * Time.deltaTime));
+            var moveDirection = new Vector3();
+
+            // Horizontal Input
+            if (inputDirection.y > 0) moveDirection += transform.forward;
+            if (inputDirection.y < 0) moveDirection -= transform.forward;
+
+            // Vertical Input
+            if (inputDirection.x > 0) moveDirection += transform.right;
+            if (inputDirection.x < 0) moveDirection -= transform.right;
+            
+            _rb.MovePosition(transform.position + moveDirection * (moveSpeed * Time.deltaTime));
         }
+
+#endregion
     }
 }
