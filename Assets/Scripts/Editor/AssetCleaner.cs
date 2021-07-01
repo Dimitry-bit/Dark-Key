04a5bbc8aa6bfceb9171e1490f9cd3f0612b4afc
Dimitry Medhat <TonyMedhat5@gmail.com>
2021-07-01 05:19:01 +0200

    Refactored 'AssetCleaner' && Added Delete Asset

--- Assets/Scripts/Editor/AssetCleaner.cs
+++ Assets/Scripts/Editor/AssetCleaner.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,275 +8,266 @@ namespace DarkKey.Editor
 {
     public class AssetCleaner : EditorWindow
     {
-        private string _destinationFolder = "Assets/Example/../..";
-        private string _assetName;
         private GameObject _model;
         private GameObject _prefab;
+        private bool _structuredFolder;
 
-        private Dictionary<Texture, string> _texturesPaths;
-        private Dictionary<Material, string> _materialPaths;
+        private HashSet<string> _texturesPaths;
+        private HashSet<string> _materialPaths;
         private string _modelPath;
         private string _prefabPath;
-        private string _assetPath;
+
+        // Optional
+        private string _destinationFolder = "Assets/Example/../..";
+        private string _assetName;
+        private string _assetNewPath;
+
+        private string _modelFolder = "Assets/Example/../..Models";
+        private string _prefabFolder = "Assets/Example/../../Prefabs";
+        private string _textureFolder = "Assets/Example/../../Textures";
+        private string _materialFolder = "Assets/Example/../../Materials";
 
         private Vector2 _scrollPos;
-        private bool _showPosition;
-
-        private string _modelFolder;
-        private string _prefabFolder;
-        private string _textureFolder;
-        private string _materialFolder;
-        private string _shaderFolder;
-
-        private Dictionary<string, string> _folderPaths;
 
         [MenuItem("Tools/Asset Cleaner")]
         public static void ShowWindow() => GetWindow<AssetCleaner>("Asset Cleaner");
 
         private void OnGUI()
         {
-            _texturesPaths = new Dictionary<Texture, string>();
-            _materialPaths = new Dictionary<Material, string>();
+            _texturesPaths = new HashSet<string>();
+            _materialPaths = new HashSet<string>();
 
-            _model = EditorGUILayout.ObjectField("Model", _model, typeof(GameObject), false) as GameObject;
-            _prefab = EditorGUILayout.ObjectField("Prefab", _prefab, typeof(GameObject), false) as GameObject;
+            DrawGui();
 
-            if (!CheckForNull()) return;
             if (!AreValidGameObjects()) return;
 
-            //_destinationFolder = EditorGUILayout.TextField("Destination (FullPath)", _destinationFolder);
-
-            _modelFolder = EditorGUILayout.TextField("Model Path", _modelFolder);
-            _prefabFolder = EditorGUILayout.TextField("Prefab Path", _prefabFolder);
-            _materialFolder = EditorGUILayout.TextField("Material Path", _materialFolder);
-            _textureFolder = EditorGUILayout.TextField("Texture Path", _textureFolder);
-
-            _folderPaths = new Dictionary<string, string>
+            if (_structuredFolder)
             {
-                {"Model", _modelFolder},
-                {"Prefab", _prefabFolder},
-                {"Material", _materialFolder},
-                {"Texture", _textureFolder}
-            };
-
-            foreach (var folder in _folderPaths)
+                if (!IsValidPath(_assetNewPath)) return;
+            }
+            else
             {
-                if (!IsValidPath(folder.Key, folder.Value)) return;
+                if (!IsValidPath(_modelFolder)) return;
+                if (!IsValidPath(_prefabFolder)) return;
+                if (!IsValidPath(_materialFolder)) return;
+                if (!IsValidPath(_textureFolder)) return;
             }
 
-            // if (!IsValidPath()) return;
+            InitializeAssetPaths();
+            DrawLogs();
+            DrawButtons();
+        }
 
-            //_assetName = EditorGUILayout.TextField("Asset Folder Name", _assetName);
-            //_assetPath = $"{_destinationFolder}/{_assetName}";
-
-            // if (string.IsNullOrEmpty(_assetName))
-            //     _assetPath = $"{_destinationFolder}/{_model.name}";
-            //
-            // EditorGUILayout.HelpBox(_assetPath, MessageType.None, false);
-
-            EditorGUILayout.Space();
-
-            if (!HasRender()) return;
-
-            // Get Model and Prefab paths
+        private void InitializeAssetPaths()
+        {
             _modelPath = AssetDatabase.GetAssetPath(_model);
             _prefabPath = AssetDatabase.GetAssetPath(_prefab);
-
-            LogAllInspector();
-
-            if (!GUILayout.Button("Move Asset")) return;
-
-            MoveAllObjectAssets();
-        }
-
-        private bool HasRender()
-        {
-            if (_prefab.TryGetComponent(out Renderer r))
+            Renderer[] renderers = GetRenderers();
+            foreach (var renderer in renderers)
             {
-                var sharedMaterials = r.sharedMaterials;
-
-                GetMaterialsPath(sharedMaterials);
-                GetTexturesPath(sharedMaterials);
-            }
-
-            if (_prefab.GetComponentsInChildren<Renderer>().Length > 0)
-            {
-                var renderers = _prefab.GetComponentsInChildren<Renderer>();
-
-                foreach (var renderer in renderers)
-                {
-                    var sharedMaterials = renderer.sharedMaterials;
-                    GetMaterialsPath(sharedMaterials);
-                    GetTexturesPath(sharedMaterials);
-                }
-
-                return true;
-            }
-
-            EditorGUILayout.HelpBox("Couldn't find a renderer", MessageType.Error);
-            return false;
-        }
-
-        private void GetMaterialsPath(Material[] sharedMaterials)
-        {
-            foreach (var material in sharedMaterials)
-            {
-                var materialPath = AssetDatabase.GetAssetPath(material);
-                if (string.IsNullOrEmpty(materialPath)) return;
-
-                RegisterMaterialPath(material, materialPath);
+                var materials = renderer.sharedMaterials;
+                _materialPaths = GetMaterialPaths(materials);
+                _texturesPaths = GetTexturePaths(materials);
             }
         }
 
-        private void GetTexturesPath(Material[] materials)
+        private Renderer[] GetRenderers()
         {
+            var renderers = new List<Renderer>();
+
+            if (_prefab.TryGetComponent(out Renderer r)) renderers.Add(r);
+            if (_prefab.GetComponentsInChildren<Renderer>().Length <= 0) return renderers.ToArray();
+
+            var childrenRenderers = _prefab.GetComponentsInChildren<Renderer>();
+            renderers.AddRange(childrenRenderers);
+            return renderers.ToArray();
+        }
+
+        private static HashSet<string> GetMaterialPaths(Material[] materials)
+        {
+            var materialPaths = new HashSet<string>();
             foreach (var material in materials)
             {
-                int[] textures = material.GetTexturePropertyNameIDs();
+                var materialPath = AssetDatabase.GetAssetPath(material);
+                if (!string.IsNullOrEmpty(materialPath)) materialPaths.Add(materialPath);
+            }
 
-                foreach (var id in textures)
+            return materialPaths;
+        }
+
+        private static HashSet<string> GetTexturePaths(Material[] materials)
+        {
+            var texturePaths = new HashSet<string>();
+            foreach (var material in materials)
+            {
+                int[] textureIds = material.GetTexturePropertyNameIDs();
+                foreach (var textureId in textureIds)
                 {
-                    var texture = material.GetTexture(id);
+                    var texture = material.GetTexture(textureId);
                     var texturePath = AssetDatabase.GetAssetPath(texture);
-
-                    if (string.IsNullOrEmpty(texturePath)) return;
-
-                    RegisterPathTexture(texture, texturePath);
+                    if (!string.IsNullOrEmpty(texturePath)) texturePaths.Add(texturePath);
                 }
             }
+
+            return texturePaths;
         }
 
-        private void RegisterPathTexture(Texture texture, string path)
+        private static void MoveObjectAsset(string oldPath, string newPath)
         {
-            if (_texturesPaths.ContainsKey(texture)) return;
-            _texturesPaths.Add(texture, path);
-        }
+            if (!AssetDatabase.IsValidFolder(newPath)) CreateFolderStructure(newPath);
 
-        private void RegisterMaterialPath(Material material, string path)
-        {
-            if (_materialPaths.ContainsKey(material)) return;
-            _materialPaths.Add(material, path);
-        }
-
-        private void MoveObjectAssets(string assetFolderName, string newFolderName, string path)
-        {
-            if (!AssetDatabase.IsValidFolder($"{_destinationFolder}/{assetFolderName}"))
-                AssetDatabase.CreateFolder($"{_destinationFolder}", $"{assetFolderName}");
-
-            if (!AssetDatabase.IsValidFolder($"{_destinationFolder}/{assetFolderName}/{newFolderName}"))
-                AssetDatabase.CreateFolder($"{_destinationFolder}/{assetFolderName}", $"{newFolderName}");
-
-            var assetName = Path.GetFileNameWithoutExtension(path);
-            var assetExtension = Path.GetExtension(path);
-
-            if (AssetDatabase.ValidateMoveAsset(path,
-                $"{_destinationFolder}/{assetFolderName}/{newFolderName}/{assetName}{assetExtension}") == "")
-                AssetDatabase.MoveAsset(path,
-                    $"{_destinationFolder}/{assetFolderName}/{newFolderName}/{assetName}{assetExtension}");
-        }
-        
-        private void MoveObjectAssets(string oldPath, string newPath)
-        {
             var assetName = Path.GetFileNameWithoutExtension(oldPath);
             var assetExtension = Path.GetExtension(oldPath);
 
             if (AssetDatabase.ValidateMoveAsset(oldPath, $"{newPath}/{assetName}{assetExtension}") == "")
                 AssetDatabase.MoveAsset(oldPath, $"{newPath}/{assetName}{assetExtension}");
         }
-        
-        private void MoveAllObjectAssets()
+
+        private static void CreateFolderStructure(string newPath)
         {
-            // Move Model
-            MoveObjectAssets(_modelPath, _modelFolder);
+            var subFolders = newPath.Split('/');
+            string subFolder = null;
+            string parentFolder = null;
 
-            // Move Prefab
-            MoveObjectAssets(_prefabPath, _prefabFolder);
-
-            // Move Materials
-            foreach (var materialPath in _materialPaths)
+            foreach (var folder in subFolders)
             {
-                MoveObjectAssets(materialPath.Value, _materialFolder);
-            }
+                subFolder += $"{folder}";
 
-            // Move Textures
-            foreach (var texturePath in _texturesPaths)
-            {
-                MoveObjectAssets(texturePath.Value, _textureFolder);
+                if (!AssetDatabase.IsValidFolder(subFolder))
+                {
+                    AssetDatabase.CreateFolder($"{parentFolder}", folder);
+                }
+
+                parentFolder = subFolder;
+                subFolder += "/";
             }
         }
 
-        // private void MoveAllObjectAssets()
-        // {
-        //     var tmpAssetName = _assetName;
-        //     if (string.IsNullOrEmpty(_assetName)) tmpAssetName = _model.name;
-        //
-        //     // Move Model
-        //     MoveObjectAssets(tmpAssetName, "Models", _modelPath);
-        //
-        //     // Move Prefab
-        //     MoveObjectAssets(tmpAssetName, "Prefabs", _prefabPath);
-        //
-        //     // Move Materials
-        //     foreach (var materialPath in _materialPaths)
-        //     {
-        //         MoveObjectAssets(tmpAssetName, "Materials", materialPath.Value);
-        //     }
-        //
-        //     // Move Textures
-        //     foreach (var texturePath in _texturesPaths)
-        //     {
-        //         MoveObjectAssets(tmpAssetName, "Textures", texturePath.Value);
-        //     }
-        // }
-
-        private void LogAllInspector()
+        private void MoveAllObjectAssets()
         {
+            MoveObjectAsset(_modelPath, _modelFolder);
+            MoveObjectAsset(_prefabPath, _prefabFolder);
+            foreach (var materialPath in _materialPaths) MoveObjectAsset(materialPath, _materialFolder);
+            foreach (var texturePath in _texturesPaths) MoveObjectAsset(texturePath, _textureFolder);
+        }
+
+        private void DeleteAllAsset()
+        {
+            AssetDatabase.DeleteAsset(_modelPath);
+            AssetDatabase.DeleteAsset(_prefabPath);
+            foreach (var materialPath in _materialPaths) AssetDatabase.DeleteAsset(materialPath);
+            foreach (var texturePath in _texturesPaths) AssetDatabase.DeleteAsset(texturePath);
+        }
+
+        private void ResetAssetCleaner()
+        {
+            _model = null;
+            _prefab = null;
+        }
+
+        private void DrawGui()
+        {
+            _model = EditorGUILayout.ObjectField("Model", _model, typeof(GameObject), false) as GameObject;
+            _prefab = EditorGUILayout.ObjectField("Prefab", _prefab, typeof(GameObject), false) as GameObject;
+            _structuredFolder = EditorGUILayout.Toggle("Structured Folder", _structuredFolder);
+
+            if (_structuredFolder)
+            {
+                _destinationFolder = EditorGUILayout.TextField("Destination (FullPath)", _destinationFolder);
+                _assetName = EditorGUILayout.TextField("Asset Folder Name", _assetName);
+
+                // Sanitize _destinationFolder
+                _assetNewPath = $"{_destinationFolder.TrimEnd('/')}";
+                
+                if (string.IsNullOrEmpty(_assetName))
+                {
+                    if (_model != null)
+                        _assetNewPath += $"/{_model.name}";
+                }
+                else
+                {
+                    _assetNewPath += $"/{_assetName}";
+                }
+
+                EditorGUILayout.HelpBox(_assetNewPath, MessageType.None, false);
+
+                _modelFolder = $"{_assetNewPath}/Models";
+                _prefabFolder = $"{_assetNewPath}/Prefabs";
+                _materialFolder = $"{_assetNewPath}/Materials";
+                _textureFolder = $"{_assetNewPath}/Textures";
+            }
+            else
+            {
+                _modelFolder = EditorGUILayout.TextField("Model Path", _modelFolder);
+                _prefabFolder = EditorGUILayout.TextField("Prefab Path", _prefabFolder);
+                _materialFolder = EditorGUILayout.TextField("Material Path", _materialFolder);
+                _textureFolder = EditorGUILayout.TextField("Texture Path", _textureFolder);
+            }
+        }
+
+        private void DrawLogs()
+        {
+            EditorGUILayout.Space();
+
             // ScrollView
             EditorGUILayout.BeginHorizontal();
-            _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos);
+            EditorGUILayout.BeginScrollView(default);
             EditorGUILayout.Space();
 
             GUI.color = Color.red;
-            GUILayout.Label("THESE FILES WILL BE MOVED", EditorStyles.boldLabel);
+            GUILayout.Label("DETECTED FILES", EditorStyles.boldLabel);
             GUI.color = Color.white;
+
             EditorGUILayout.Space();
 
-            GUI.color = Color.green;
-
             // Log Path
-            EditorGUILayout.HelpBox($" [ModelPath]: ({_modelPath})", MessageType.None);
-            EditorGUILayout.HelpBox($" [PrefabPath]: ({_prefabPath})", MessageType.None);
-
-            foreach (var texturesPath in _texturesPaths)
-                EditorGUILayout.HelpBox($" [TexturePath]: ({texturesPath.Value})", MessageType.None);
+            GUI.color = Color.green;
+            EditorGUILayout.HelpBox($" [ModelPath]: {_modelPath} --> {_modelFolder}", MessageType.None);
+            EditorGUILayout.HelpBox($" [PrefabPath]: {_prefabPath} --> {_prefabFolder}", MessageType.None);
 
             foreach (var materialPath in _materialPaths)
-                EditorGUILayout.HelpBox($" [MaterialPath]: ({materialPath.Value})", MessageType.None);
+                EditorGUILayout.HelpBox($" [MaterialPath]: {materialPath} --> {_materialFolder}", MessageType.None);
+
+            foreach (var texturesPath in _texturesPaths)
+                EditorGUILayout.HelpBox($" [TexturePath]: {texturesPath} --> {_textureFolder}", MessageType.None);
 
             GUI.color = Color.white;
-
             EditorGUILayout.EndScrollView();
             EditorGUILayout.EndHorizontal();
         }
 
-        private bool CheckForNull()
+        private void DrawButtons()
         {
-            if (_model != null && _prefab != null) return true;
-            EditorGUILayout.HelpBox("MODEL OR PREFAB IS MISSING", MessageType.Error);
-            return false;
+            if (GUILayout.Button("Move Asset"))
+            {
+                MoveAllObjectAssets();
+                ResetAssetCleaner();
+            }
+
+            if (GUILayout.Button("Delete Asset"))
+            {
+                DeleteAllAsset();
+                ResetAssetCleaner();
+            }
         }
 
-        private bool IsValidPath(string type, string path)
+        private static bool IsValidPath(string path)
         {
-            if (AssetDatabase.IsValidFolder(path))
-                return true;
+            var pathRegex = new Regex(@"^Assets(/){1}([0-9a-zA-Z-_\s/.]*/?)*/?$");
+            if (pathRegex.IsMatch(path)) return true;
 
-            EditorGUILayout.HelpBox($"Invalid {type} PATH.", MessageType.Error);
+            EditorGUILayout.HelpBox($"Invalid Path, Path must start with \"Assets/\".", MessageType.Error);
             return false;
         }
 
         private bool AreValidGameObjects()
         {
+            if (_model == null || _prefab == null)
+            {
+                EditorGUILayout.HelpBox("MODEL OR PREFAB IS MISSING", MessageType.Error);
+                return false;
+            }
+
             if (PrefabUtility.GetPrefabAssetType(_model) != PrefabAssetType.Model)
             {
                 EditorGUILayout.HelpBox("INVALID MODEL. PLEASE REASSIGN MODEL", MessageType.Error);
