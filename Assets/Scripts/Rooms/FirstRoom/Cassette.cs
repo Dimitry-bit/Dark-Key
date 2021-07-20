9c000c1317aaac843c9fdad7ef5ace056191bd0d
Arsany <arsanyhany69@gmail.com>
2021-07-20 02:09:13 +0200

    Fixed Cassette Script Again!

--- Assets/Scripts/Rooms/FirstRoom/Cassette.cs
+++ Assets/Scripts/Rooms/FirstRoom/Cassette.cs
@@ -7,16 +7,17 @@ namespace DarkKey
     public class Cassette : MonoBehaviour
     {
         private CD _cdObject;
-        void Start()
-        {
-            _cdObject = new CD();
-        }
+        private bool hasCD;
         void OnCollisionEnter(Collision collidedObject)
         {
+            if (hasCD) return;
+            _cdObject = collidedObject.gameObject.GetComponent<CD>();
+            
             //Checks if the collided object is a CD
             if (collidedObject.gameObject.layer == 10)
             {
                 _cdObject.PlayAudio();
+                hasCD = true;
             }
         }
     }
