2d66ea89f4f8b031ec8b2d904f948203a6b42298
Dimitry <TonyMedhat5@gmail.com>
2021-07-21 02:38:13 +0200

    Updated 'GameManager'
    Added StartGame To GameManger
    Added Debugging to GameManger

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,20 +1,46 @@
-﻿using UnityEngine;
+﻿using DarkKey.Core.Debugger;
+using MLAPI.SceneManagement;
+using UnityEngine;
 
 namespace DarkKey.Core
 {
     public class GameManager : MonoBehaviour
     {
-        public static void QuitGame()
+        private static readonly DebugLogLevel[] ScriptLogLevel = {DebugLogLevel.Core};
+
+        [Header("Scene Settings")]
+        [SerializeField] private string onlineScene = "Multiplayer_Test";
+
+        private static GameManager _instance;
+        public static GameManager Instance
         {
-           Application.Quit(); 
+            get
+            {
+                if (_instance == null) CustomDebugger.LogError("NetPortal", "Instance is null", ScriptLogLevel);
+                return _instance;
+            }
         }
 
-        public static void PauseGame()
+        #region Unity Methods
+
+        private void Awake()
         {
+            if (_instance == null)
+                _instance = this;
+            else if (_instance != this)
+               Destroy(gameObject); 
+
+            DontDestroyOnLoad(gameObject);
         }
 
-        public static void SaveGame()
-        {
-        }
+        #endregion
+        
+        public void StartGame() => NetworkSceneManager.SwitchScene(onlineScene);
+
+        public static void QuitGame() => Application.Quit();
+
+        public static  void PauseGame() => CustomDebugger.LogInfo("GameManager", "Game paused.", ScriptLogLevel);
+        
+        public static void SaveGame() => CustomDebugger.LogInfo("GameManager", "Game saved.", ScriptLogLevel);
     }
 }
