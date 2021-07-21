f90de20a414c34856af38c65c8db08d2c304c4ed
Dimitry <TonyMedhat5@gmail.com>
2021-07-21 02:39:10 +0200

    Created 'PlayerData' And 'PlayerSpawnPoint'

---
+++ Assets/Scripts/Gameplay/PlayerData.cs
@@ -0,0 +1,25 @@
+﻿using MLAPI.Serialization;
+
+namespace DarkKey.Gameplay
+{
+    public struct PlayerData : INetworkSerializable
+    {
+        public ulong ClientId;
+        public string Name;
+        public string Role;
+
+        public PlayerData(ulong clientId, string name, string role)
+        {
+            ClientId = clientId;
+            Name = name;
+            Role = role;
+        }
+
+        public void NetworkSerialize(NetworkSerializer serializer)
+        {
+            serializer.Serialize(ref ClientId);
+            serializer.Serialize(ref Name);
+            serializer.Serialize(ref Role);
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Spawn/PlayerSpawnPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace DarkKey.Gameplay.Spawn
+{
+    public class PlayerSpawnPoint : MonoBehaviour
+    {
+        private void Awake() => PlayerSpawner.AddSpawnPoint(transform);
+        private void OnDestroy() => PlayerSpawner.RemoveSpawnPoint(transform);
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.black;
+            Gizmos.DrawSphere(transform.position, 1f);
+            Gizmos.color = Color.green; 
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward * 2);
+        }
+    }
+}
