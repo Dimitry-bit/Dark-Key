b056e4522aa1d331f1d4fe6a9ed6c5f44633d602
Dimitry <73913361+Dimitry-bit@users.noreply.github.com>
2021-07-23 23:34:27 +0200

    Project Cleanup

---
+++ Assets/Scripts/Ui/Billboard.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using DarkKey.Core.Debugger;
+
+namespace DarkKey.Ui
+{
+    public class Billboard : MonoBehaviour
+    {
+        [SerializeField] private Camera mainCamera;
+
+        #region Unity Functions
+
+        private void Start()
+        {
+            if (mainCamera != null) return;
+
+            mainCamera = Camera.main;
+            CustomDebugger.LogWarning("Billboard", $"mainCamera variable is not assigned | Defaulted to Camera.Main",
+                new[]
+                {
+                    DebugLogLevel.UI
+                });
+        }
+
+        private void LateUpdate() => transform.LookAt(transform.position + mainCamera.transform.forward);
+
+        #endregion
+    }
+}
