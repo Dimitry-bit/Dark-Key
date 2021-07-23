7dc056aca2f45b0d957cd986f52e6b571fda2f26
Arsany <arsanyhany69@gmail.com>
2021-07-23 10:36:10 +0200

    Added the gramophone

--- Assets/Scripts/Rooms/FirstRoom/Cassette.cs
+++ Assets/Scripts/Rooms/FirstRoom/Cassette.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DarkKey.Gameplay;
+using DarkKey.Gameplay.Interfaces;
 
 namespace DarkKey
 {
-    public class Cassette : MonoBehaviour
+    public class Cassette : MonoBehaviour, IInteractable
     {
         private CD _cdObject;
         private bool hasCD;
@@ -12,12 +14,32 @@ namespace DarkKey
         {
             if (hasCD) return;
             _cdObject = collidedObject.gameObject.GetComponent<CD>();
-            
+
             //Checks if the collided object is a CD
             if (collidedObject.gameObject.layer == 10)
             {
                 _cdObject.PlayAudio();
                 hasCD = true;
+            }
+        }
+        public virtual void Interact(Player player)
+        {
+            if (!hasCD)
+            {
+                if (player.IsHoldingItem() && player.ItemInHand.Value.gameObject.layer == 10)
+                {
+                    player.RemoveItemFromHand();
+                    _cdObject = player.ItemInHand.Value.GetComponent<CD>();
+                    _cdObject.PlayAudio();
+                    hasCD = true;
+                }
+            }
+            else
+            {
+                if (!player.IsHoldingItem())
+                {
+                    player.AssignItemToHand();
+                }
             }
         }
     }
