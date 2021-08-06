562b8e2fca9780c977f314b398e5a7f5d0c4a4f9
Dimitry <73913361+Dimitry-bit@users.noreply.github.com>
2021-08-06 21:57:45 +0200

    Fixed 'SceneSwitcher.cs (LevelSelector.cs)' && 'DebugBar.cs'

--- Assets/Scripts/Ui/Debug Panels/DebugBar.cs
+++ Assets/Scripts/Ui/Debug Panels/DebugBar.cs
@@ -1,75 +1,69 @@
-// using DarkKey.Core.Managers;
-// using DarkKey.Gameplay;
-// using Mirror;
-// using UnityEngine;
-//
-// namespace DarkKey.Ui.Debug_Panels
-// {
-//     public class DebugBar : NetworkBehaviour
-//     {
-//         [SerializeField] private GameObject debugBar;
-//         private InputHandler _inputHandler;
-//         private bool _isPanelEnabled;
-//
-//         private void Update()
-//         {
-//             if (_inputHandler == null)
-//             {
-//                 if (Input.GetKeyDown(KeyCode.BackQuote))
-//                     ShowMenu();
-//             }
-//         }
-//
-//         private void ShowMenu()
-//         {
-//             if (_isPanelEnabled)
-//                 DisablePanel();
-//             else
-//                 EnablePanel();
-//         }
-//
-//         private void EnablePanel()
-//         {
-//             if (_inputHandler == null)
-//                 GetInputHandlerAndAssignEvent();
-//
-//             if (_inputHandler != null)
-//                 _inputHandler.actionMap = InputHandler.InputActionMap.Ui;
-//
-//             CursorManager.ShowCursor();
-//             debugBar.SetActive(true);
-//             _isPanelEnabled = true;
-//         }
-//
-//         private void DisablePanel()
-//         {
-//             if (_inputHandler == null)
-//                 GetInputHandlerAndAssignEvent();
-//
-//             if (_inputHandler != null)
-//                 _inputHandler.actionMap = InputHandler.InputActionMap.Gameplay;
-//
-//             CursorManager.HideCursor();
-//             debugBar.SetActive(false);
-//             _isPanelEnabled = false;
-//         }
-//
-//         private void GetInputHandlerAndAssignEvent()
-//         {
-//             if (NetworkManager.Singleton == null) return;
-//             foreach (var networkClient in NetworkManager.Singleton.ConnectedClientsList)
-//             {
-//                 if (networkClient.PlayerObject == null) continue;
-//                 if (networkClient.PlayerObject.IsLocalPlayer)
-//                 {
-//                     var inputHandler = networkClient.PlayerObject.GetComponent<InputHandler>();
-//                     if (inputHandler != null)
-//                     {
-//                         _inputHandler = inputHandler;
-//                         _inputHandler.OnConsole += ShowMenu;
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
+using DarkKey.Core.Managers;
+using DarkKey.Gameplay;
+using Mirror;
+using UnityEngine;
+
+namespace DarkKey.Ui.Debug_Panels
+{
+    public class DebugBar : MonoBehaviour
+    {
+        [SerializeField] private GameObject debugBar;
+        private InputHandler _inputHandler;
+        private bool _isPanelEnabled;
+
+        private void Update()
+        {
+            if (_inputHandler == null)
+            {
+                if (Input.GetKeyDown(KeyCode.BackQuote))
+                    ShowMenu();
+            }
+        }
+
+        private void ShowMenu()
+        {
+            if (_isPanelEnabled)
+                DisablePanel();
+            else
+                EnablePanel();
+        }
+
+        private void EnablePanel()
+        {
+            if (_inputHandler == null)
+                GetInputHandlerAndAssignEvent();
+
+            if (_inputHandler != null)
+                _inputHandler.actionMap = InputHandler.InputActionMap.Ui;
+
+            CursorManager.ShowCursor();
+            debugBar.SetActive(true);
+            _isPanelEnabled = true;
+        }
+
+        private void DisablePanel()
+        {
+            if (_inputHandler == null)
+                GetInputHandlerAndAssignEvent();
+
+            if (_inputHandler != null)
+                _inputHandler.actionMap = InputHandler.InputActionMap.Gameplay;
+
+            CursorManager.HideCursor();
+            debugBar.SetActive(false);
+            _isPanelEnabled = false;
+        }
+
+        private void GetInputHandlerAndAssignEvent()
+        {
+            if (NetworkManager.singleton == null || !NetworkManager.singleton.isNetworkActive) return;
+
+            NetworkIdentity localPlayer = NetworkClient.localPlayer;
+            if (localPlayer != null && localPlayer.TryGetComponent(out InputHandler inputHandler))
+            {
+                _inputHandler = inputHandler;
+                _inputHandler.OnConsole += ShowMenu;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Ui/Debug Panels/SceneSwitcher.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+using System.IO;
+using DarkKey.Core.Debugger;
+using DarkKey.Core.Managers;
+using TMPro;
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DarkKey.Ui.Debug_Panels
+{
+    public class SceneSwitcher : BaseDebugPanel
+    {
+        private static readonly DebugLogLevel[] ScriptLogLevel = {DebugLogLevel.UI};
+
+        [Header("Options")]
+        [SerializeField] private GameObject buttonPrefab;
+        [SerializeField] [Scene] private string[] switchableScenes;
+        private readonly List<Button> _buttons = new List<Button>();
+
+        #region Unity Methods
+
+        private void OnDestroy() => UnSubscribeAllButtons();
+
+        #endregion
+
+        #region Private Methods
+
+        protected override void EnablePanel()
+        {
+            base.EnablePanel();
+
+            if (!isServer) return;
+
+            if (_buttons.Count != 0) return;
+
+            ServiceLocator.Instance.customDebugger.LogInfo("Try to fetch level data", ScriptLogLevel);
+            InitializePanel();
+        }
+
+        protected override void InitializePanel()
+        {
+            if (NetworkManager.singleton == null || !NetworkManager.singleton.isNetworkActive) return;
+
+            foreach (var scenePath in switchableScenes)
+            {
+                ServiceLocator.Instance.customDebugger.LogInfo($"Initializing => ({scenePath})", ScriptLogLevel);
+                string sceneName = Path.GetFileNameWithoutExtension(scenePath);
+                CreateButton(sceneName);
+            }
+
+            SubscribeAllButtons();
+        }
+
+        private void CreateButton(string buttonName)
+        {
+            GameObject buttonGameObject = Instantiate(buttonPrefab, Vector3.zero, Quaternion.identity, gridTransform);
+            Button buttonScript = buttonGameObject.GetComponent<Button>();
+            TMP_Text tmp = buttonScript.GetComponentInChildren<TMP_Text>();
+
+            buttonGameObject.name = buttonName;
+            tmp.text = buttonName;
+
+            _buttons.Add(buttonScript);
+        }
+
+        private void SubscribeAllButtons()
+        {
+            foreach (var button in _buttons) SubscribeToEvent(button);
+        }
+
+        private void UnSubscribeAllButtons()
+        {
+            foreach (var button in _buttons) UnSubscribeToEvent(button);
+        }
+
+        private void SubscribeToEvent(Button button)
+        {
+            if (button != null)
+                button.onClick.AddListener(() => ChangeLevel(button.name));
+        }
+
+        private void UnSubscribeToEvent(Button button)
+        {
+            if (button != null)
+                button.onClick.RemoveAllListeners();
+        }
+
+        private void ChangeLevel(string levelName)
+        {
+            CmdChangeLevel(connectionToServer.connectionId, levelName);
+        }
+
+        [Command]
+        private void CmdChangeLevel(int clientConnectionId, string levelName)
+        {
+            if (NetworkServer.connections.ContainsKey(clientConnectionId))
+            {
+                NetworkIdentity client = NetworkServer.connections[clientConnectionId].identity;
+                if (client == null || !client.isLocalPlayer) return;
+            }
+
+            ServiceLocator.Instance.networkSceneManager.SwitchScene(levelName);
+        }
+
+        #endregion
+    }
+}
