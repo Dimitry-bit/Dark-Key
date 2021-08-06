a6654c8b815fc7be31207a6ac3475074a61660df
Dimitry <73913361+Dimitry-bit@users.noreply.github.com>
2021-08-06 16:35:40 +0200

    Refactored 'PlayerSpawner.cs' && 'PlayerSpawnPoint.cs' to work with Mirror-API

--- Assets/Scripts/Gameplay/Spawn/PlayerSpawnPoint.cs
+++ Assets/Scripts/Gameplay/Spawn/PlayerSpawnPoint.cs
@@ -1,18 +1,20 @@
-﻿// using UnityEngine;
-//
-// namespace DarkKey.Gameplay.Spawn
-// {
-//     public class PlayerSpawnPoint : MonoBehaviour
-//     {
-//         private void Awake() => PlayerSpawner.AddSpawnPoint(transform);
-//         private void OnDestroy() => PlayerSpawner.RemoveSpawnPoint(transform);
-//
-//         private void OnDrawGizmos()
-//         {
-//             Gizmos.color = Color.black;
-//             Gizmos.DrawSphere(transform.position, 1f);
-//             Gizmos.color = Color.green; 
-//             Gizmos.DrawLine(transform.position, transform.position + transform.forward * 2);
-//         }
-//     }
-// }
+﻿using UnityEngine;
+
+namespace DarkKey.Gameplay.Spawn
+{
+    public class PlayerSpawnPoint : MonoBehaviour
+    {
+        private void Awake() => PlayerSpawner.AddSpawnPoint(transform);
+        private void OnDestroy() => PlayerSpawner.RemoveSpawnPoint(transform);
+
+        private void OnDrawGizmos()
+        {
+            var currentPosition = transform.position;
+
+            Gizmos.color = Color.black;
+            Gizmos.DrawSphere(currentPosition, 1f);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(currentPosition, currentPosition + transform.forward * 2);
+        }
+    }
+}
--- Assets/Scripts/Gameplay/Spawn/PlayerSpawner.cs
+++ Assets/Scripts/Gameplay/Spawn/PlayerSpawner.cs
@@ -1,95 +1,115 @@
-﻿// using System.Collections.Generic;
-// using System.Linq;
-// using DarkKey.Core.Debugger;
-// using DarkKey.Core.Network;
-// using DarkKey.Gameplay.CorePlayer;
-// using Mirror;
-// using UnityEngine;
-//
-// namespace DarkKey.Gameplay.Spawn
-// {
-//     public class PlayerSpawner : NetworkBehaviour
-//     {
-//         private static readonly DebugLogLevel[] ScriptLogLevel = {DebugLogLevel.Core};
-//
-//         [SerializeField] private NetworkIdentity playerPrefab;
-//
-//         private static List<Transform> _spawnPoints = new List<Transform>();
-//         private int _spawnPointIndex;
-//
-//         #region Unity Methods
-//
-//         public void NetworkStart()
-//         {
-//             CustomDebugger.LogInfo("Network Start", ScriptLogLevel);
-//             NetPortal.Instance.OnSceneSwitch += CmdSpawnPlayer;
-//         }
-//
-//         private void OnDestroy()
-//         {
-//             if (NetPortal.Instance != null)
-//                 NetPortal.Instance.OnSceneSwitch -= CmdSpawnPlayer;
-//         }
-//
-//         #endregion
-//
-//         #region Public Methods
-//
-//         public static void AddSpawnPoint(Transform transform)
-//         {
-//             _spawnPoints.Add(transform);
-//             _spawnPoints = _spawnPoints.OrderBy(x => x.GetSiblingIndex()).ToList();
-//         }
-//
-//         public static void RemoveSpawnPoint(Transform transform) => _spawnPoints.Remove(transform);
-//
-//         #endregion
-//
-//         #region Private Methods
-//
-//         [Command]
-//         private void CmdSpawnPlayer(PlayerData playerData)
-//         {
-//             DestroyPlayerObject(playerData.ClientId);
-//
-//             var spawnPoint = GetSpawnPoint();
-//
-//             InstantiatePlayer(playerData, spawnPoint);
-//
-//             _spawnPointIndex++;
-//
-//             CustomDebugger.LogInfo("Player spawned successfully.", ScriptLogLevel);
-//         }
-//
-//         private Transform GetSpawnPoint()
-//         {
-//             var spawnPoint = _spawnPoints[_spawnPointIndex];
-//
-//             if (spawnPoint != null) return spawnPoint;
-//
-//             CustomDebugger.LogError($"Missing spawn point for player {_spawnPointIndex}",
-//                 ScriptLogLevel);
-//
-//             return null;
-//         }
-//
-//         private void InstantiatePlayer(PlayerData playerData, Transform spawnPointTransform)
-//         {
-//             var position = spawnPointTransform.position;
-//             var rotation = spawnPointTransform.rotation;
-//
-//             NetworkIdentity playerInstance = Instantiate(playerPrefab, position, rotation);
-//             // TODO: Add Player.Name and Player.Role
-//             playerInstance.GetComponent<NetworkIdentity>().SpawnAsPlayerObject(playerData.ClientId);
-//             playerInstance.GetComponent<ScenePlayer>().InitializePlayerData(playerData);
-//         }
-//
-//         private void DestroyPlayerObject(ulong clientId)
-//         {
-//             if (NetworkSpawnManager.GetPlayerNetworkObject(clientId) != null)
-//                 NetworkSpawnManager.GetPlayerNetworkObject(clientId).Despawn(true);
-//         }
-//
-//         #endregion
-//     }
-// }
+﻿using System.Collections.Generic;
+using System.Linq;
+using DarkKey.Core.Debugger;
+using DarkKey.Core.Managers;
+using DarkKey.Core.Network;
+using Mirror;
+using UnityEngine;
+
+namespace DarkKey.Gameplay.Spawn
+{
+    public class PlayerSpawner : NetworkBehaviour
+    {
+        private static readonly DebugLogLevel[] ScriptLogLevel = {DebugLogLevel.Core};
+
+        [SerializeField] private bool autoSpawnPlayerOnSceneStart = true;
+
+        [SerializeField] private NetworkIdentity playerPrefab;
+        private static List<Transform> _spawnPoints = new List<Transform>();
+        private int _spawnPointIndex;
+
+        #region Unity Methods
+
+        public override void OnStartServer()
+        {
+            if (!autoSpawnPlayerOnSceneStart) return;
+            Invoke(nameof(ReplaceAllPlayersGameObjects), 1f);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public static void AddSpawnPoint(Transform transform)
+        {
+            _spawnPoints.Add(transform);
+            _spawnPoints = _spawnPoints.OrderBy(x => x.GetSiblingIndex()).ToList();
+        }
+
+        public static void RemoveSpawnPoint(Transform transform) => _spawnPoints.Remove(transform);
+
+        #endregion
+
+        #region Private Methods
+
+        private Transform GetSpawnPoint()
+        {
+            var spawnPoint = _spawnPoints[_spawnPointIndex];
+
+            if (spawnPoint != null) return spawnPoint;
+
+            ServiceLocator.Instance.customDebugger.LogError($"Missing spawn point for player {_spawnPointIndex}",
+                ScriptLogLevel);
+
+            return null;
+        }
+
+        private void ReplacePlayerGameObject(NetworkIdentity oldPlayer, NetworkIdentity newPlayer)
+        {
+            Transform spawnPoint = GetSpawnPoint();
+            var position = spawnPoint.position;
+            var rotation = spawnPoint.rotation;
+
+            if (newPlayer == null || oldPlayer == null)
+            {
+                var messageDiff = oldPlayer == null ? "oldPlayer" : "newPlayer";
+
+                ServiceLocator.Instance.customDebugger.LogError(
+                    $"Failed to spawn player ({messageDiff} can't be null).", ScriptLogLevel);
+                return;
+            }
+
+            var isSpawned = NetworkServer.ReplacePlayerForConnection(oldPlayer.connectionToClient,
+                Instantiate(newPlayer.gameObject, position, rotation));
+
+            if (!isSpawned)
+            {
+                ServiceLocator.Instance.customDebugger.LogError("Failed to spawn player.", ScriptLogLevel);
+                return;
+            }
+
+            ServiceLocator.Instance.customDebugger.LogInfo("Player spawned successfully.", ScriptLogLevel);
+
+            // TODO: Add Player.Name && Player.Role.
+            // TODO: Initialize PlayerData in newly created playerGameObject. 
+
+            _spawnPointIndex++;
+        }
+
+        private void DestroyPlayer(GameObject playerGameObject)
+        {
+            string playerGameObjectName = playerGameObject.name;
+            NetworkServer.Destroy(playerGameObject);
+
+            ServiceLocator.Instance.customDebugger.LogInfo($"{playerGameObjectName} Destroyed successfully.",
+                ScriptLogLevel);
+        }
+
+        private void ReplaceAllPlayersGameObjects()
+        {
+            ServiceLocator.Instance.customDebugger.LogInfo("Started replacing all players GameObjects.",
+                ScriptLogLevel);
+
+            foreach (var lobbyPlayer in NetPortal.Instance.LobbyPlayers)
+            {
+                ReplacePlayerGameObject(lobbyPlayer.netIdentity, playerPrefab);
+                DestroyPlayer(lobbyPlayer.gameObject);
+            }
+
+            ServiceLocator.Instance.customDebugger.LogInfo("Finished replacing all players GameObjects.",
+                ScriptLogLevel);
+        }
+
+        #endregion
+    }
+}
