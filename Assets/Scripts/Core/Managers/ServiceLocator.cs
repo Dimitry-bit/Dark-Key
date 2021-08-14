9102ae3e2868ef2208039e76ab712a9e2afdc518
Dimitry <73913361+Dimitry-bit@users.noreply.github.com>
2021-08-15 01:32:00 +0200

    Before new 'ServiceManager.cs' Implementation.

--- Assets/Scripts/Core/Managers/ServiceLocator.cs
+++ Assets/Scripts/Core/Managers/ServiceLocator.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using DarkKey.Core.Debugger;
 using DarkKey.Core.Network;
 using DarkKey.Ui.Pages;
@@ -8,18 +10,121 @@ namespace DarkKey.Core.Managers
     public class ServiceLocator : MonoBehaviour
     {
         public static ServiceLocator Instance { get; private set; }
+        private Dictionary<Type, MonoBehaviour> _services;
 
         public NetworkSceneManagerDk networkSceneManager;
         public CustomDebugger customDebugger;
         public PageController pageController;
         public GameManager gameManager;
 
+        #region Unity Methods
+
         private void Awake()
+        {
+            SingletonInitialize();
+            ServicesInitialize();
+        }
+
+        private void ServicesInitialize()
+        {
+            _services = new Dictionary<Type, MonoBehaviour>();
+
+            var customDebuggers = GetDebugger();
+            var networkSceneManagerDk = GetNetworkSceneManager();
+            var gameManagers = GetGameManager();
+            var pageControllers = GetPageController();
+
+            _services.Add(typeof(CustomDebugger), customDebuggers);
+            _services.Add(typeof(NetworkSceneManagerDk), networkSceneManagerDk);
+            _services.Add(typeof(GameManager), gameManagers);
+            _services.Add(typeof(PageController), pageControllers);
+
+            foreach (var service in _services.Values)
+            {
+                if (service == null) return;
+                DontDestroyOnLoad(service.transform.root);
+            }
+        }
+
+        private void SingletonInitialize()
         {
             if (Instance == null)
                 Instance = this;
             else if (Instance != this)
                 Destroy(this);
+
+            DontDestroyOnLoad(transform.root);
         }
+
+        #endregion
+
+        #region Public Methods
+
+        public CustomDebugger GetDebugger()
+        {
+            if (_services.ContainsKey(typeof(CustomDebugger)))
+                return (CustomDebugger) _services[typeof(CustomDebugger)];
+
+            if (TryFindService(typeof(CustomDebugger), out MonoBehaviour service))
+                return (CustomDebugger) service;
+
+            return null;
+        }
+
+        public GameManager GetGameManager()
+        {
+            if (_services.ContainsKey(typeof(GameManager)))
+                return (GameManager) _services[typeof(GameManager)];
+
+            if (TryFindService(typeof(GameManager), out MonoBehaviour service))
+                return (GameManager) service;
+
+            return null;
+        }
+
+        public NetworkSceneManagerDk GetNetworkSceneManager()
+        {
+            if (_services.ContainsKey(typeof(NetworkSceneManagerDk)))
+                return (NetworkSceneManagerDk) _services[typeof(NetworkSceneManagerDk)];
+
+            if (TryFindService(typeof(NetworkSceneManagerDk), out MonoBehaviour service))
+                return (NetworkSceneManagerDk) service;
+
+            return null;
+        }
+
+        public PageController GetPageController()
+        {
+            if (_services.ContainsKey(typeof(PageController)))
+                return (PageController) _services[typeof(PageController)];
+
+            if (TryFindService(typeof(PageController), out MonoBehaviour service))
+                return (PageController) service;
+
+            return null;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private bool TryFindService(Type serviceType, out MonoBehaviour service)
+        {
+            MonoBehaviour foundService = (MonoBehaviour) FindObjectOfType(serviceType);
+            Debug.Log(foundService.name);
+
+            service = null;
+
+            if (foundService != null)
+            {
+                service = foundService;
+                return true;
+            }
+
+            Debug.LogError("Couldn't find service.");
+            return false;
+        }
+
+        #endregion
     }
 }
